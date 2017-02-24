41ec07d4480d3a89b3c07480faa75457873be721
Michael Bateman <michael.bateman.com@gmail.com>
2017-02-24 20:19:38 +0000

    added tests

--- first/assignment1.erl
+++ first/assignment1.erl
@@ -1,28 +1,42 @@
 -module(assignment1).
--export[perimeter/1,area/1,enclose/1,bits/1,recursive_bits/1].
+-export[perimeter/1,area/1,enclose/1,bits/1,recursive_bits/1,test_perimeter/0,test_area/0,test_enclose/0,test_bits/0].
 
 perimeter({square, Side}) ->
-    io:format("perimeter for square is ~p~n", [4*Side]);
+    4*Side;
 perimeter({rectangle, A, B}) ->
-    io:format("perimeter for rectangle is ~p~n", [2*(A+B)]);
+    2*(A+B);
 perimeter({circle, Radius}) ->
-    io:format("perimeter for circle is ~p~n", [2*math:pi()*Radius]);
+    2*math:pi()*Radius;
 perimeter({triangle, A, B, C}) ->
-    io:format("perimeter for triangle ~p~n", [A + B + C]);
+    A + B + C;
 perimeter(_) ->
-    io:format("unrecognised shape ~n", []).
+     0.
+
+test_perimeter() ->
+    40 = perimeter({square, 10}),
+    50 = perimeter({rectangle, 10, 15}),
+    62.83185307179586 = perimeter({circle, 10}),
+    85 = perimeter({triangle, 15, 30, 40}),
+    success.
 
 area({square, Side}) ->
-    io:format("area for square is ~p~n", [Side*Side]);
+    Side*Side;
 area({rectangle, A, B}) ->
-    io:format("area for rectangle is ~p~n", [A*B]);
+    A*B;
 area({circle, Radius}) ->
-    io:format("area for circle is ~p~n", [math:pi()*(Radius*Radius)]);
+    math:pi()*(Radius*Radius);
 area({triangle, A, B, C}) ->
     S = (A+B+C)/2,
-    io:format("area for triangle ~p~n", [math:sqrt(S*(S-A)*(S-B)*(S-C))]);
+    math:sqrt(S*(S-A)*(S-B)*(S-C));
 area(_) ->
-    io:format("unrecognised shape ~n", []).
+    0.
+
+test_area() ->
+    100 = area({square, 10}),
+    150 = area({rectangle, 10, 15}),
+    314.1592653589793 = area({circle, 10}),
+    191.1110606427582 = area({triangle, 15, 30, 40}),
+    success.
 
 triplemax(A,B,C) ->
     max(max(A,B),max(B,C)).
@@ -38,11 +52,16 @@ triangle_height({triangle, A, B, C}) ->
     2*triangle_area({triangle, A, B, C})/triplemax(A,B,C).
 
 enclose({circle, R}) ->
-    io:format("enclosing rectangle for circle is ~p~n", [{rectangle,2*R,2*R}]);
+    {rectangle,2*R,2*R};
 enclose({triangle, A, B, C}) ->
-    io:format("enclosing rectangle for triangle is ~p~n", [{rectangle, triplemax(A,B,C), triangle_height({triangle,A,B,C})}]);
+    {rectangle, triplemax(A,B,C), triangle_height({triangle,A,B,C})};
 enclose(_) ->
-    io:format("unrecognised shape ~n", []).
+    0.
+
+test_enclose() ->
+    {rectangle,20,20} = enclose({circle, 10}),
+    {rectangle,40,9.55555303213791} = enclose({triangle, 15, 30, 40}),
+    success.
 
 bits(N) when N < 10 ->
     <<B4:1,B3:1,B2:1,B1:1>> = <<N:4>>,
@@ -58,3 +77,9 @@ recursive_bits(0, A) ->
 recursive_bits(N, A) ->
     recursive_bits(N div 10, A + bits(N rem 10)).
 
+test_bits() ->
+    3 = bits(7),
+    1 = bits(8),
+    3 = recursive_bits(7),
+    1 = recursive_bits(8),
+    success.
